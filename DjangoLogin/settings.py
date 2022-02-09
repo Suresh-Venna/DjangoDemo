ba2a659efda6f149cad5717a8ba7321e15d4eba3
Venkata Suresh Venna <venkatasureshvenna@gmail.com>
2022-02-09 11:43:17 -0600

    Docker file added and app got dockerized

--- DjangoLogin/settings.py
+++ DjangoLogin/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'DjangoLogin.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ["C:\\Users\\vvenn\\Desktop\\Django_demo\\.vscode\\templates"],
+        'DIRS': [".vscode/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
